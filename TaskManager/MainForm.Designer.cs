3e1bdd8859d7856cc80a6c886b76546e42f2ae5e
JacKira <SilverMaiker@gmail.com>
2020-09-17 18:18:47 +0300

    First steps

---
+++ TaskManager/MainForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace TaskManager
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NavigationMenu = new System.Windows.Forms.GroupBox();
+            this.TaskTable = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.TaskTable.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // NavigationMenu
+            // 
+            this.NavigationMenu.Location = new System.Drawing.Point(12, 12);
+            this.NavigationMenu.Name = "NavigationMenu";
+            this.NavigationMenu.Size = new System.Drawing.Size(271, 748);
+            this.NavigationMenu.TabIndex = 0;
+            this.NavigationMenu.TabStop = false;
+            this.NavigationMenu.Text = "Menu";
+            this.NavigationMenu.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // TaskTable
+            // 
+            this.TaskTable.AutoScroll = true;
+            this.TaskTable.AutoSize = true;
+            this.TaskTable.BackColor = System.Drawing.SystemColors.Control;
+            this.TaskTable.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.TaskTable.ColumnCount = 3;
+            this.TaskTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 337F));
+            this.TaskTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.TaskTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 328F));
+            this.TaskTable.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.TaskTable.Location = new System.Drawing.Point(309, 12);
+            this.TaskTable.Name = "TaskTable";
+            this.TaskTable.RowCount = 3;
+            this.TaskTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 49.69072F));
+            this.TaskTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 258F));
+            this.TaskTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 247F));
+            this.TaskTable.Size = new System.Drawing.Size(995, 785);
+            this.TaskTable.TabIndex = 1;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.BackColor = System.Drawing.SystemColors.Window;
+            this.tableLayoutPanel2.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18.18182F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 81.81818F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(330, 260);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1348, 801);
+            this.Controls.Add(this.TaskTable);
+            this.Controls.Add(this.NavigationMenu);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.TaskTable.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox NavigationMenu;
+        private System.Windows.Forms.TableLayoutPanel TaskTable;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+    }
+}
+
---
+++ TaskManager/MainForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskManager
+{
+
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Task_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            TaskTable.Controls.Add(InitNote(), 1, 0);
+            TaskTable.Controls.Add(InitNote(), 2, 0);
+
+            TaskTable.Controls.Add(InitNote(), 1, 1);
+            TaskTable.Controls.Add(InitNote(), 2, 1);
+            TaskTable.Controls.Add(InitNote(), 0, 1);
+            
+
+        }
+
+        private System.Windows.Forms.TableLayoutPanel InitNote()
+        {
+            var NewNote = new System.Windows.Forms.TableLayoutPanel();
+            NewNote.BackColor = System.Drawing.SystemColors.Window;
+            NewNote.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            NewNote.ColumnCount = 1;
+            NewNote.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            NewNote.Location = new System.Drawing.Point(3, 3);
+            NewNote.Name = "tableLayoutPanel2";
+            NewNote.RowCount = 4;
+            NewNote.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18.18182F));
+            NewNote.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 81.81818F));
+            NewNote.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            NewNote.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            NewNote.Size = new System.Drawing.Size(330, 260);
+            NewNote.TabIndex = 0;
+            return NewNote;
+        }
+
+        private void AddNote()
+        {
+                    
+        }
+    }
+}
