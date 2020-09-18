d828a6cff133355fe5f5a986fa38e8e99c1b76c0
JacKira <SilverMaiker@gmail.com>
2020-09-18 16:37:38 +0300

    Upgrade interface

--- TaskManager/MainForm.Designer.cs
+++ TaskManager/MainForm.Designer.cs
@@ -30,6 +30,14 @@
         {
             this.NavigationMenu = new System.Windows.Forms.GroupBox();
             this.TaskTable = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textbox = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.TaskTable.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // NavigationMenu
@@ -51,12 +59,90 @@
             this.TaskTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.TaskTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.TaskTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.TaskTable.Controls.Add(this.tableLayoutPanel1, 0, 0);
             this.TaskTable.Location = new System.Drawing.Point(289, 12);
             this.TaskTable.Name = "TaskTable";
             this.TaskTable.RowCount = 1;
             this.TaskTable.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.TaskTable.Size = new System.Drawing.Size(1047, 748);
             this.TaskTable.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.Window;
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.textbox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.textBox5, 0, 4);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18.18182F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 81.81818F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(330, 320);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // textbox
+            // 
+            this.textbox.BackColor = System.Drawing.SystemColors.Window;
+            this.textbox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textbox.Location = new System.Drawing.Point(4, 4);
+            this.textbox.Multiline = true;
+            this.textbox.Name = "textbox";
+            this.textbox.Size = new System.Drawing.Size(322, 33);
+            this.textbox.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.Location = new System.Drawing.Point(4, 44);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(322, 172);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox3.Location = new System.Drawing.Point(4, 223);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(322, 22);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox4.Location = new System.Drawing.Point(4, 252);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(322, 27);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.textBox5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox5.Location = new System.Drawing.Point(4, 286);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(322, 30);
+            this.textBox5.TabIndex = 5;
+            this.textBox5.Text = "CLICK ME";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            
             // 
             // MainForm
             // 
@@ -68,6 +154,9 @@
             this.Name = "MainForm";
             this.Text = "Task Table";
             this.Load += new System.EventHandler(this.MainForm_Load);
+            this.TaskTable.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -77,6 +166,12 @@
         private System.Windows.Forms.GroupBox NavigationMenu;
         private System.Windows.Forms.TableLayoutPanel TaskTable;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textbox;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
 
--- TaskManager/MainForm.cs
+++ TaskManager/MainForm.cs
@@ -35,8 +35,9 @@ namespace TaskManager
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            for(int i = 0; i < 10; i++)
-                AddNote("Test task","Need execute some task forrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr",
+            tableLayoutPanel1.Controls[0].Text = "Test task";
+            for (int i = 0; i < 10; i++)
+                AddNote("Test task", "Need execute some task forrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr",
                 "Me", "Active");
 
         }
@@ -52,19 +53,21 @@ namespace TaskManager
             NewNote.ColumnCount = 1;
             NewNote.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             NewNote.Location = new System.Drawing.Point(3, 3);
-            NewNote.Name = "tableLayoutPanel" + (last_note_cords[0] + last_note_cords[1] + 1).ToString();
-            NewNote.RowCount = 4;
+            NewNote.Name = "tableLayoutPanel" + (last_note_cords.Sum() + 1).ToString();
+            NewNote.RowCount = 5;
             NewNote.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18.18182F));
             NewNote.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 81.81818F));
-            NewNote.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
             NewNote.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
-            NewNote.Size = new System.Drawing.Size(330, 260);
+            NewNote.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
+            NewNote.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            NewNote.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            NewNote.Size = new System.Drawing.Size(330, 320);
             NewNote.TabIndex = 0;
             var textbox = new System.Windows.Forms.TextBox()
             {
                 BackColor = Color.White,
                 Multiline = true,
-                ReadOnly = true,
+                
                 Text = title,
                 Width = 260,
                 Size = new System.Drawing.Size(330, 30)
@@ -76,7 +79,7 @@ namespace TaskManager
             {
                 BackColor = Color.White,
                 Multiline = true,
-                ReadOnly = true,
+                
                 Text = description,
                 WordWrap = true,
                 ScrollBars = ScrollBars.Vertical,
@@ -88,7 +91,7 @@ namespace TaskManager
             textbox = new System.Windows.Forms.TextBox()
             {
                 BackColor = Color.White,
-                ReadOnly = true,
+                
                 Text = employer,
                 Width = 260,
                 Size = new System.Drawing.Size(330, 20),
@@ -99,13 +102,27 @@ namespace TaskManager
             textbox = new System.Windows.Forms.TextBox()
             {
                 BackColor = Color.White,
-                ReadOnly = true,
+                
                 Text = status,
                 Width = 260,
                 Size = new System.Drawing.Size(330, 20)
             };
             HideCaret(textbox.Handle);
             NewNote.Controls.Add(textbox);
+
+            NewNote.Controls.Add(new System.Windows.Forms.TextBox()
+            {
+                BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0))))),
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                Location = new System.Drawing.Point(4, 286),
+                Multiline = true,
+                ReadOnly = true,
+                Size = new System.Drawing.Size(322, 30),
+                Text = "CLICK ME",
+                TextAlign = HorizontalAlignment.Center,
+                TabIndex = 5
+            });
+            NewNote.Controls[4].ContextMenuStrip = new NoteContextMenu();
             return NewNote;
         }
 
@@ -114,25 +131,42 @@ namespace TaskManager
         {
             int row = last_note_cords[0];
             int col = last_note_cords[1];
-            if ((row == 0) && (col == 0))
-            {
-                TaskTable.Controls.Add(InitNote(title, description, employer, status), col, row);
-                return;
-            }
+            /*    if ((row == 0) && (col == 0))
+                {
+                    TaskTable.Controls.Add(InitNote(title, description, employer, status), col, row);
+                    return;
+                } */
             if (col == 2)
             {
                 row++;
                 col = 0;
-                TaskTable.RowCount++;
+                last_note_cords[0] = row;
+                last_note_cords[1] = col;
                 TaskTable.Controls.Add(InitNote(title, description, employer, status), col, row);
             }
             else
             {
                 col++;
+                last_note_cords[0] = row;
+                last_note_cords[1] = col;
                 TaskTable.Controls.Add(InitNote(title, description, employer, status), col, row);
             }
-            last_note_cords[0] = row;
-            last_note_cords[1] = col;
+
         }
+
+        private class NoteContextMenu : System.Windows.Forms.ContextMenuStrip
+        {
+            public NoteContextMenu()
+            {
+                this.Items.Add(new ToolStripMenuItem("Удалить"));
+            }
+        }
+
+        private void RemoveNote(System.Windows.Forms.TableLayoutPanel note)
+        {
+            note.Dispose();
+        }
+
+      
     }
 }
