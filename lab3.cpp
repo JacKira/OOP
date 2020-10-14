080fdda2b8f4b2d8b31ed06afc5165811b02ba16
Кирилл Фейзуллин <SilverMaiker@gmail.com>
2020-10-14 09:20:40 +0300

    Upgrade removing, pop, push and counter

--- lab3.cpp
+++ lab3.cpp
@@ -19,7 +19,7 @@ public:
 class Stack
 {
 private:
-	int length = 1;
+	int count = 0;
 public:
 	MyData data;
 	Stack* ptr;
@@ -27,6 +27,7 @@ public:
 	{
 		data = a;
 		ptr = NULL;
+		count = 1;
 	};
 	Stack()
 	{
@@ -41,14 +42,23 @@ public:
 	void push(const MyData& d);
 	MyData pop();
 	void RemoveAll();
+	void PrintStack();
+	int GetCount();
 };
 
 int main(int argc, char* argv[])
 {
 	MyData a(1);
-	Stack s(a);
-	for(int i = 0; i < 100000; i++)
+	Stack s;
+	
+	cout << s.GetCount() << endl;
+	for (int i = 0; i < 100000; i++)
 		s.push(a);
+	cout << s.GetCount() << endl;
+	for (int i = 0; i < 99995; i++)
+		s -= a;
+	cout << s.GetCount() << endl;
+	s.PrintStack();
 	system("pause");
 	s.~Stack();
 	system("pause");
@@ -110,33 +120,48 @@ Stack operator-(const Stack& s, const MyData& d)
 
 Stack& Stack ::operator-=(const MyData& d)
 {
-	pop();
+	if (data == d) {
+		pop();
+	}
 	return *this;
 }
 
 void Stack::push(const MyData& d)
 {
-	Stack* p = new Stack(this->data);
-	(*p).ptr = this->ptr;
-	(*p).length = 0;
-	this->length++;
-	this->data = d;
-	this->ptr = p;
+	if (count > 0) {
+		Stack* p = new Stack(this->data);
+		(*p).ptr = this->ptr;
+		(*p).count = -1;
+		this->count++;
+		this->data = d;
+		this->ptr = p;
+	}
+	else {
+		this->data = d;
+		this->count = 1;
+	}
 }
 
 MyData Stack::pop()
 {
 	MyData a = this->data;
-	Stack* p = ptr->ptr;
-	MyData d = ptr->data;
-	delete this->ptr;
-	this->data = d;
-	this->ptr = p;
+	if (count > 1) {
+		Stack* p = ptr->ptr;
+		MyData d = ptr->data;
+		delete this->ptr;
+		this->data = d;
+		this->ptr = p;
+	}
+	else {
+		this->ptr = NULL;
+	}
+	this->count--;
 	return a;
 };
 
-void Stack::RemoveAll() {
-	Stack* cptr, * pptr;
+void Stack::RemoveAll()
+{
+	Stack* cptr, *pptr;
 	cptr = this->ptr;
 	while (cptr != NULL)
 	{
@@ -146,10 +171,26 @@ void Stack::RemoveAll() {
 		pptr = NULL;
 	}
 	this->ptr = NULL;
+	this->count = 0;
 }
 
-Stack :: ~Stack() {
-	if (length > 1) {
+Stack :: ~Stack()
+{
+	if (count > 0) {
 		RemoveAll();
 	}
+}
+
+void Stack::PrintStack()
+{
+	while (this->count)
+	{
+		pop().PrintData();
+	} 
+}
+
+int Stack::GetCount()
+{
+	int n = count;
+	return n;
 }
