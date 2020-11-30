09b6bfe5821d353fdac954b5e14c978f682f08a1
JacKira <47638256+JacKira@users.noreply.github.com>
2020-11-30 13:17:45 +0300

    Last update

--- Program/ObjectPool.cpp
+++ Program/ObjectPool.cpp
@@ -12,6 +12,7 @@ class ObjectPool
         static ObjectPool* instance;
         ObjectPool() {}
     public:
+        //Статический метод для получения единого экземпляра класса
         static ObjectPool* getInstance()
         {
             if (instance == NULL)
@@ -21,6 +22,7 @@ class ObjectPool
             return instance;
         }
       
+        //Метод, которые выдает объект из пула, если есть свободный
         Artist* getResource()
         {
 
@@ -35,6 +37,7 @@ class ObjectPool
                 
         }
        
+        //Метод вовзрата объекта в пул
         void returnResource(Artist* object)
         {
             if (resources.GetCount() < max) {
@@ -48,6 +51,7 @@ class ObjectPool
             }
         }
 
+        //Метод получения количества свободных объектов пуле
         int GetPoolCount()
         {
             int count = resources.GetCount();
@@ -56,25 +60,30 @@ class ObjectPool
 
 
 };
+
 ObjectPool* ObjectPool::instance = 0;
+
+
 int main()
 {
     setlocale(LC_ALL, "RUS");
-    ifstream fin("data.txt");
-    ObjectPool* pool = ObjectPool::getInstance();
+    ifstream fin("data.txt", ios::in);
+    ObjectPool* pool = ObjectPool::getInstance(); //Получаем единственный на всю программу экзмепляр пула
     
     while(fin) {
         Artist* one;
-        one = pool->getResource();
-        one->InputDataRowFromFileTxt(fin);
-        one->PrintData();
-        pool->returnResource(one);
+        one = pool->getResource(); //Получаем объект из пула
+        one->InputDataRowFromFileTxt(fin); //Записываем данные в объект
+        one->PrintData(); //Печатаем результат
+        pool->returnResource(one); //Возваращаем объект обратно в пул
     }
     fin.close();
 
+    //Симулируем нарушение работы пула, пытаясь вернуть лишний объект
     Artist* disturber = new Artist;
-    pool->returnResource(disturber);
+    pool->returnResource(disturber); //Здесь нам сообщат об ошибки и объект не добавится
  
+    //Покажем, что все получаемые объекты изначально пусты
     cout << endl;
     while (pool->GetPoolCount()) {
         Artist* one;
@@ -82,6 +91,7 @@ int main()
         one->PrintData();
     }
 
+    //Симулируем нарушение работы пула, попыткой получить объект из пустого пула
     disturber = pool->getResource();
     disturber->PrintData();
 
