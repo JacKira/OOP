a417faee55b771666754d343b25af9fb6c937a33
8Andre8 <boing1999@mail.ru>
2020-12-08 14:04:56 +0300

    DBgrid

--- TaskManager/MainForm.Designer.cs
+++ TaskManager/MainForm.Designer.cs
@@ -48,10 +48,12 @@
             this.StatusButton1 = new System.Windows.Forms.RadioButton();
             this.EmployersLabel = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.contextMenuStrip1.SuspendLayout();
             this.toolStrip1.SuspendLayout();
             this.FilterMenu.SuspendLayout();
             this.StatusMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // TaskTable
@@ -66,46 +68,49 @@
             this.TaskTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.TaskTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.TaskTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.TaskTable.Location = new System.Drawing.Point(289, 63);
+            this.TaskTable.Location = new System.Drawing.Point(385, 78);
+            this.TaskTable.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TaskTable.Name = "TaskTable";
             this.TaskTable.RowCount = 1;
             this.TaskTable.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.TaskTable.Size = new System.Drawing.Size(1058, 748);
+            this.TaskTable.Size = new System.Drawing.Size(1411, 921);
             this.TaskTable.TabIndex = 1;
             // 
             // contextMenuStrip1
             // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.addNoteToolStripMenuItem,
             this.testToolStripMenuItem});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(124, 48);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(141, 52);
             // 
             // addNoteToolStripMenuItem
             // 
             this.addNoteToolStripMenuItem.Name = "addNoteToolStripMenuItem";
-            this.addNoteToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.addNoteToolStripMenuItem.Size = new System.Drawing.Size(140, 24);
             this.addNoteToolStripMenuItem.Text = "Add note";
             this.addNoteToolStripMenuItem.Click += new System.EventHandler(this.addNoteToolStripMenuItem_Click);
             // 
             // testToolStripMenuItem
             // 
             this.testToolStripMenuItem.Name = "testToolStripMenuItem";
-            this.testToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.testToolStripMenuItem.Size = new System.Drawing.Size(140, 24);
             this.testToolStripMenuItem.Text = "Test";
             this.testToolStripMenuItem.Click += new System.EventHandler(this.testToolStripMenuItem_Click);
             // 
             // toolStrip1
             // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripButton1,
             this.toolStripButton2,
             this.toolStripButton3,
             this.toolStripButton4});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-            this.toolStrip1.MinimumSize = new System.Drawing.Size(0, 50);
+            this.toolStrip1.MinimumSize = new System.Drawing.Size(0, 62);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(1357, 50);
+            this.toolStrip1.Size = new System.Drawing.Size(1809, 62);
             this.toolStrip1.TabIndex = 2;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -115,7 +120,7 @@
             this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
             this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(23, 47);
+            this.toolStripButton1.Size = new System.Drawing.Size(29, 59);
             this.toolStripButton1.Text = "toolStripButton1";
             // 
             // toolStripButton2
@@ -124,7 +129,7 @@
             this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
             this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton2.Name = "toolStripButton2";
-            this.toolStripButton2.Size = new System.Drawing.Size(23, 47);
+            this.toolStripButton2.Size = new System.Drawing.Size(29, 59);
             this.toolStripButton2.Text = "toolStripButton2";
             // 
             // toolStripButton3
@@ -133,7 +138,7 @@
             this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
             this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton3.Name = "toolStripButton3";
-            this.toolStripButton3.Size = new System.Drawing.Size(23, 47);
+            this.toolStripButton3.Size = new System.Drawing.Size(29, 59);
             this.toolStripButton3.Text = "toolStripButton3";
             // 
             // toolStripButton4
@@ -142,21 +147,24 @@
             this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
             this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton4.Name = "toolStripButton4";
-            this.toolStripButton4.Size = new System.Drawing.Size(23, 47);
+            this.toolStripButton4.Size = new System.Drawing.Size(29, 59);
             this.toolStripButton4.Text = "toolStripButton4";
             // 
             // FilterMenu
             // 
             this.FilterMenu.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.FilterMenu.BackgroundImage = global::TaskManager.Properties.Resources.kaban2;
+            this.FilterMenu.Controls.Add(this.dataGridView1);
             this.FilterMenu.Controls.Add(this.SearchLabel);
             this.FilterMenu.Controls.Add(this.SearchTextBox);
             this.FilterMenu.Controls.Add(this.StatusMenu);
             this.FilterMenu.Controls.Add(this.EmployersLabel);
             this.FilterMenu.Controls.Add(this.comboBox1);
-            this.FilterMenu.Location = new System.Drawing.Point(12, 63);
+            this.FilterMenu.Location = new System.Drawing.Point(16, 78);
+            this.FilterMenu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.FilterMenu.Name = "FilterMenu";
-            this.FilterMenu.Size = new System.Drawing.Size(271, 748);
+            this.FilterMenu.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.FilterMenu.Size = new System.Drawing.Size(361, 921);
             this.FilterMenu.TabIndex = 0;
             this.FilterMenu.TabStop = false;
             this.FilterMenu.Text = "Фильтры";
@@ -166,17 +174,19 @@
             // 
             this.SearchLabel.AutoSize = true;
             this.SearchLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.SearchLabel.Location = new System.Drawing.Point(6, 36);
+            this.SearchLabel.Location = new System.Drawing.Point(8, 44);
+            this.SearchLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.SearchLabel.Name = "SearchLabel";
-            this.SearchLabel.Size = new System.Drawing.Size(141, 17);
+            this.SearchLabel.Size = new System.Drawing.Size(179, 20);
             this.SearchLabel.TabIndex = 7;
             this.SearchLabel.Text = "Поиск по заголовку:";
             // 
             // SearchTextBox
             // 
-            this.SearchTextBox.Location = new System.Drawing.Point(3, 56);
+            this.SearchTextBox.Location = new System.Drawing.Point(4, 69);
+            this.SearchTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.SearchTextBox.Name = "SearchTextBox";
-            this.SearchTextBox.Size = new System.Drawing.Size(262, 20);
+            this.SearchTextBox.Size = new System.Drawing.Size(348, 22);
             this.SearchTextBox.TabIndex = 6;
             // 
             // StatusMenu
@@ -186,9 +196,11 @@
             this.StatusMenu.Controls.Add(this.StatusButton1);
             this.StatusMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.StatusMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.StatusMenu.Location = new System.Drawing.Point(6, 165);
+            this.StatusMenu.Location = new System.Drawing.Point(8, 203);
+            this.StatusMenu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.StatusMenu.Name = "StatusMenu";
-            this.StatusMenu.Size = new System.Drawing.Size(259, 100);
+            this.StatusMenu.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.StatusMenu.Size = new System.Drawing.Size(345, 123);
             this.StatusMenu.TabIndex = 5;
             this.StatusMenu.TabStop = false;
             this.StatusMenu.Text = "Статус задачи";
@@ -196,9 +208,10 @@
             // StatusButton3
             // 
             this.StatusButton3.AutoSize = true;
-            this.StatusButton3.Location = new System.Drawing.Point(11, 73);
+            this.StatusButton3.Location = new System.Drawing.Point(15, 90);
+            this.StatusButton3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.StatusButton3.Name = "StatusButton3";
-            this.StatusButton3.Size = new System.Drawing.Size(101, 21);
+            this.StatusButton3.Size = new System.Drawing.Size(124, 24);
             this.StatusButton3.TabIndex = 2;
             this.StatusButton3.TabStop = true;
             this.StatusButton3.Text = "Выполнено";
@@ -207,9 +220,10 @@
             // StatusButton2
             // 
             this.StatusButton2.AutoSize = true;
-            this.StatusButton2.Location = new System.Drawing.Point(11, 46);
+            this.StatusButton2.Location = new System.Drawing.Point(15, 57);
+            this.StatusButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.StatusButton2.Name = "StatusButton2";
-            this.StatusButton2.Size = new System.Drawing.Size(86, 21);
+            this.StatusButton2.Size = new System.Drawing.Size(107, 24);
             this.StatusButton2.TabIndex = 1;
             this.StatusButton2.TabStop = true;
             this.StatusButton2.Text = "В работе";
@@ -218,9 +232,10 @@
             // StatusButton1
             // 
             this.StatusButton1.AutoSize = true;
-            this.StatusButton1.Location = new System.Drawing.Point(11, 19);
+            this.StatusButton1.Location = new System.Drawing.Point(15, 23);
+            this.StatusButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.StatusButton1.Name = "StatusButton1";
-            this.StatusButton1.Size = new System.Drawing.Size(125, 21);
+            this.StatusButton1.Size = new System.Drawing.Size(159, 24);
             this.StatusButton1.TabIndex = 0;
             this.StatusButton1.TabStop = true;
             this.StatusButton1.Text = "Нужно сделать";
@@ -230,29 +245,42 @@
             // 
             this.EmployersLabel.AutoSize = true;
             this.EmployersLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.EmployersLabel.Location = new System.Drawing.Point(6, 94);
+            this.EmployersLabel.Location = new System.Drawing.Point(8, 116);
+            this.EmployersLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.EmployersLabel.Name = "EmployersLabel";
-            this.EmployersLabel.Size = new System.Drawing.Size(82, 17);
+            this.EmployersLabel.Size = new System.Drawing.Size(105, 20);
             this.EmployersLabel.TabIndex = 4;
             this.EmployersLabel.Text = "Сотрудник:";
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(6, 114);
+            this.comboBox1.Location = new System.Drawing.Point(8, 140);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(259, 21);
+            this.comboBox1.Size = new System.Drawing.Size(344, 24);
             this.comboBox1.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(92, 726);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 8;
             // 
             // TaskTableForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.ClientSize = new System.Drawing.Size(1357, 856);
+            this.ClientSize = new System.Drawing.Size(1809, 1054);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.TaskTable);
             this.Controls.Add(this.FilterMenu);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "TaskTableForm";
             this.Text = "Task Table";
             this.TransparencyKey = System.Drawing.SystemColors.Highlight;
@@ -265,6 +293,7 @@
             this.FilterMenu.PerformLayout();
             this.StatusMenu.ResumeLayout(false);
             this.StatusMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -291,6 +320,7 @@
         private System.Windows.Forms.RadioButton StatusButton1;
         private System.Windows.Forms.TextBox SearchTextBox;
         private System.Windows.Forms.Label SearchLabel;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
